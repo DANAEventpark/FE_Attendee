d35ca7a9ce83c0faa3588005786747ecc78ee07d
manhnguyen270724-sudo <manh.nguyen270724@gmail.com>
2026-05-16 16:09:10 +0700

    feat(FE-ATT): adjust register page ratio to 50:50

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -77,8 +77,9 @@ const RegisterPage = () => {
 
   return (
     <div className="flex flex-col lg:flex-row min-h-screen w-full">
-      {/* LEFT COLUMN - FORM (30% on desktop) */}
-      <div className="w-full lg:w-[30%] flex flex-col justify-center px-6 py-10 sm:px-12 lg:px-16 bg-white order-2 lg:order-1">
+      {/* LEFT COLUMN - FORM (50% on desktop) */}
+      <div className="w-full lg:w-[50%] flex flex-col justify-center px-6 py-10 sm:px-16 lg:px-24 bg-white order-2 lg:order-1">
+
 
 
         <div className="max-w-md w-full mx-auto">
@@ -194,8 +195,9 @@ const RegisterPage = () => {
         </div>
       </div>
 
-      {/* RIGHT COLUMN - IMAGE (70% on desktop) */}
-      <div className="w-full h-[45vh] lg:h-auto lg:w-[70%] relative flex flex-col p-6 lg:p-12 order-1 lg:order-2 shrink-0">
+      {/* RIGHT COLUMN - IMAGE (50% on desktop) */}
+
+      <div className="w-full h-[45vh] lg:h-auto lg:w-[50%] relative flex flex-col p-6 lg:p-12 order-1 lg:order-2 shrink-0">
 
 
         {/* Background Image */}
