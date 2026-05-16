a879416fb8ac883887ca4fbdae9d742de35abd1d
manhnguyen270724-sudo <manh.nguyen270724@gmail.com>
2026-05-16 15:53:31 +0700

    feat(FE-ATT): build register page for attendee [REQ_03]

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import ProtectedRoute from './components/ProtectedRoute';
+
 
 // Dummy component for events to prevent errors
 const EventsPage = () => <div className="p-8 text-2xl font-bold">Attendee Events Page</div>;
@@ -12,6 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
       <Route 
         path="/events" 
         element={
